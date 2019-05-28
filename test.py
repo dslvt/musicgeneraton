ed8cd4010f7723f2a4da5263092e415bb13ac612
Dinmukhametov Salavat <salavatarus@gmail.com>
2019-05-28 19:22:19 +0300

    random node generation

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 #%%
 import pygame 
-import pyFluidSynth
+from midiutil import MIDIFile
+import random as rd
+# import pyFluidSynth
 #%%
 def play_music(music_file):
     """
@@ -19,24 +21,68 @@ def play_music(music_file):
         # check if playback has finished
         clock.tick(30)
 
-#%%
-music_file = "input.mid"
 
-freq = 44100    # audio CD quality
-bitsize = -16   # unsigned 16 bit
-channels = 2    # 1 is mono, 2 is stereo
-buffer = 1024    # number of samples
-pygame.mixer.init(freq, bitsize, channels, buffer)
-# optional volume 0 to 1.0
-pygame.mixer.music.set_volume(0.8)
-try:
-    # use the midi file you just saved
-    play_music(music_file)
-except KeyboardInterrupt:
-    # if user hits Ctrl/C then exit
-    # (works only in console mode)
-    pygame.mixer.music.fadeout(1000)
-    pygame.mixer.music.stop()
-    raise SystemExit
+#%%
+def playMusicWithStandartSettings(music_file):
+    music_file = music_file
+
+    freq = 44100    # audio CD quality
+    bitsize = -16   # unsigned 16 bit
+    channels = 2    # 1 is mono, 2 is stereo
+    buffer = 1024    # number of samples
+    pygame.mixer.init(freq, bitsize, channels, buffer)
+    # optional volume 0 to 1.0
+    pygame.mixer.music.set_volume(0.8)
+    try:
+        # use the midi file you just saved
+        play_music(music_file)
+    except KeyboardInterrupt:
+        # if user hits Ctrl/C then exit
+        # (works only in console mode)
+        pygame.mixer.music.fadeout(1000)
+        pygame.mixer.music.stop()
+        raise SystemExit
+
+#%%
+get_random_midi(10)
+playMusicWithStandartSettings('random.mid')
+# degrees  = [60, 62, 64, 65, 67, 69, 71, 72]  # MIDI note number
+# track    = 0
+# channel  = 0
+# time     = 0    # In beats
+# duration = 1    # In beats
+# tempo    = 60   # In BPM
+# volume   = 100  # 0-127, as per the MIDI standard
+
+# MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created
+#                       # automatically)
+# MyMIDI.addTempo(track, time, tempo)
+
+# for i, pitch in enumerate(degrees):
+#     MyMIDI.addNote(track, channel, pitch, time + i, duration, volume)
+
+# with open("major-scale.mid", "wb") as output_file:
+#     MyMIDI.writeFile(output_file)
+
+#%%
+def get_random_midi(n):
+    degrees = [] 
+    for i in range(n):
+       degrees.append(rd.randint(60, 80)) 
+    
+    print(degrees)
+    track = 0
+    channel = 0
+    time = 0
+    duration = 1
+    tempo = 60 
+    volume = 100
+    Midi = MIDIFile(1)
+    Midi.addTempo(track, time, tempo)
+    for i, pitch in enumerate(degrees):
+        Midi.addNote(track, channel, pitch, time+i, duration, volume)
+
+    with open("random.mid", "wb") as output_file:
+        Midi.writeFile(output_file)
 
 #%%
